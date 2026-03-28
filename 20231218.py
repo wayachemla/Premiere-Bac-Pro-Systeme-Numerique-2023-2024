a7e5f7e057d3be661be8506e325ff8fcf19d482d
Waya CHEMLA <147428192+wayachemla@users.noreply.github.com>
2026-03-28 14:19:57 +0100

    modif

---
+++ 20231218.py
@@ -0,0 +1,100 @@
+# TP 2 Python - Première Bac Pro SN
+
+# print("Hello World")
+
+"""
+print("Bonjour")
+print("Bonjour!")
+"""
+
+"""
+print("Bonjour")
+print("Je m'appelle Hélène")
+print("Je suis une fille")
+print("Comme les autres")
+"""
+
+"""
+print("Un")
+print("Deux")
+print("Trois")
+print("Soleil")
+"""
+
+"""
+print("Un", end="")
+print("Deux", end="")
+print("Trois", end="")
+print()
+print("Soleil")
+"""
+
+"""
+monAge = 25
+print("J'ai", monAge, "!")
+"""
+
+"""
+distance = 55758000
+print(distance)
+print(2 * distance)
+"""
+
+"""
+contenance = 60
+print("Départ :", contenance)
+contenance = 100
+print("Remplissage :", contenance)
+contenance = contenance - 15
+print("Consommation :", contenance)
+"""
+
+# Calcul du nombre total de frères et sœurs
+nombreDeFrere = 2
+nombreDeSoeur = 0
+print(nombreDeFrere + nombreDeSoeur)
+
+"""
+monAge = 15
+monAge = 16
+print(monAge)
+"""
+
+"""
+age = 15
+age = age + 1
+print(age)
+"""
+
+"""
+# Boucle de punition ou répétition
+for i in range(101):
+    print("Il faut écouter la prof en cours ElectroTP")
+"""
+
+"""
+for i in range(11):
+    print("Salut")
+print("A bientôt")
+"""
+
+"""
+# Table de multiplication
+for loop in range(13):
+    print("9 * 8 = 72")
+"""
+
+"""
+n = 1000
+for i in range(1, n):
+    print(i)
+print("Il fait beau")
+"""
+
+# Compte à rebours pour le décollage
+n = 1002
+
+for i in range(1, n):
+    print(1001 - i)
+
+print("Décollage")
